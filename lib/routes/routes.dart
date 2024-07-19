658be8c0ca5c646e299ef3e72b237914a5a1e085
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-19 09:10:11 +0530

    Update routes.dart

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,2 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../view/HomeScreen.dart';
+
+class MyRoutes{
+  static Map<String, Widget Function(BuildContext)> myRoutes = {
+    '/' : (context) => const HomeScreen(),
+  };
+}
