d9d5ff081ac56e6be74b835fa038d723a9b03111
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-07-23 10:31:31 +0530

    Update routes.dart

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:json_parsing/screen/Lec-8.3/view/post_screen.dart';
 
+import '../Lec-8.2/view/user_screen.dart';
 import '../view/HomeScreen.dart';
 
 class MyRoutes{
   static Map<String, Widget Function(BuildContext)> myRoutes = {
-    '/' : (context) => const HomeScreen(),
+    '/' : (context) => const PostScreen(),
+    '/user': (context) => const UserScreen(),
+    '/Home': (context) => const HomeScreen(),
   };
 }
